9d89ff47c3a30dc6ff3d947814bea5df7ea6135a
Jan Behrens <dev@jpbehrens.de>
2021-12-24 19:26:56 +0100

    update

--- src/MyCalendar.js
+++ src/MyCalendar.js
@@ -38,46 +38,49 @@ export class MyCalendar {
         return button;
     }
 
-    uiDisplayMonth() {
-        const month = document.createElement('div');
-        const monthName = document.createElement('div');
-        monthName.innerHTML = this._month[this.lang][this.date.getMonth()];
-        const btnNextMonth = this.uiChangeButton({ txt: 'Next Month', date: this.calculateNewDate({ month: 1 }) });
-        const btnLastMonth = this.uiChangeButton({ txt: 'Last Month', date: this.calculateNewDate({ month: -1 }) });
+    uiHeaderControll(type) {
 
-        month.appendChild(btnLastMonth)
-        month.appendChild(monthName)
-        month.appendChild(btnNextMonth)
+        const possibleTypes = ['day','month','year'];
+        const DATE = this.date;
+        const wrap = document.createElement('div');
+        wrap.className = 'myc-head-control'
+        if (!possibleTypes.includes(type)) {
+            wrap.innerHTML = "ERROR";
+            console.warn('Wrong Type used');
+            return wrap;
+        }
 
-        return month;
+        const labelTxt = {
+            day: DATE.getDate(),
+            month: this._month[this.lang][DATE.getMonth()],
+            year: DATE.getFullYear()
+        }
+
+        const label = document.createElement('div');
+        label.innerHTML = labelTxt[type];
+        const btnNext = this.uiChangeButton({ txt: `Next ${type}`, date: this.calculateNewDate({ [type]: 1 }) });
+        const btnLast = this.uiChangeButton({ txt: `Last ${type}`, date: this.calculateNewDate({ [type]: -1 }) });
+
+        wrap.appendChild(btnLast)
+        wrap.appendChild(label)
+        wrap.appendChild(btnNext)
+
+        return wrap;
     }
-
-    uiDisplayYear() {
-        const month = document.createElement('div');
-        const monthName = document.createElement('div');
-        monthName.innerHTML = this.date.getFullYear();
-        const btnNextMonth = this.uiChangeButton({ txt: 'Next Year', date: this.calculateNewDate({ year: 1 }) });
-        const btnLastMonth = this.uiChangeButton({ txt: 'Last Year', date: this.calculateNewDate({ year: -1 }) });
-
-        month.appendChild(btnLastMonth)
-        month.appendChild(monthName)
-        month.appendChild(btnNextMonth)
-
-        return month;
-    }
-
 
     uiHeader() {
         const header = document.createElement('div');
-        header.classList.add('myc-header')
-        const displayMonth = this.uiDisplayMonth();
-        const displayYear = this.uiDisplayYear();
+        header.classList.add('myc-header');
 
-        header.appendChild(displayMonth);
-        header.appendChild(displayYear);
+        header.appendChild(this.uiHeaderControll('day'));
+        header.appendChild(this.uiHeaderControll('month'));
+        header.appendChild(this.uiHeaderControll('year'));
 
         return header;
     }
+
+
+
 
     uiSelect() {
         const select = document.createElement('div');
@@ -98,18 +101,18 @@ export class MyCalendar {
         tenBack.innerHTML = '-10';
         tenBack.className = 'myc-subselect';
         tenBack.onclick = () => {
-            this.changeDate(this.calculateNewDate({year: -10}));
+            this.changeDate(this.calculateNewDate({ year: -10 }));
         }
         select.appendChild(tenBack);
 
         for (let ii = start; ii < end; ii++) {
 
             const subSelect = document.createElement('div');
-            const year = this.calculateNewDate({year: ii-this.date.getFullYear()})
-            
+            const year = this.calculateNewDate({ year: ii - this.date.getFullYear() })
+
             subSelect.className = this.date.getFullYear() === year.getFullYear()
-            ? 'myc-subselect-active'
-            : 'myc-subselect';
+                ? 'myc-subselect-active'
+                : 'myc-subselect';
 
             subSelect.innerHTML = year.getFullYear();
             subSelect.onclick = () => {
@@ -123,7 +126,7 @@ export class MyCalendar {
         tenForward.innerHTML = '+10';
         tenForward.className = 'myc-subselect';
         tenForward.onclick = () => {
-            this.changeDate(this.calculateNewDate({year: 10}));
+            this.changeDate(this.calculateNewDate({ year: 10 }));
         }
         select.appendChild(tenForward);
 
